0b3fc4f423438857a84ac4f3f9d2817af3a62a15
Mohamed Youssfi <med@youssfi.net>
2023-06-25 09:43:22 +0100

    FC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'enset-app';
+  actions : Array<any> =[
+    {title :"Home", "route":"/home", icon : "house"},
+    {title :"Products", "route":"/products", icon : "search"},
+    {title :"New Product", "route":"/newProduct", icon : "safe"}
+  ];
+  currentAction :any;
+
+  setCurrentAction(action: any) {
+    this.currentAction = action;
+  }
 }
